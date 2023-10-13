792b299e4642c5691ff2cf32807c8708305110e6
iqbal <iqbalumarr@gmail.com>
2023-10-13 10:36:59 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "---------------------------------------------------------------------" << endl;
+    cout << "Nama: Iqbal Umar" << endl << endl;
+    cout << "NPM: 2310631170091" << endl << endl;
+    cout << "Kelas: A-Informatika" << endl <<endl;
+    cout << "Cita cita: fullstack developer" << endl <<endl;
+    cout << "Motivasi masuk Informatika: Melihat perkembangan teknologi yang pesat" <<endl <<endl;
+    cout << "dan prospek pekerjaan yang luas" <<endl;
+    cout << "---------------------------------------------------------------------" << endl;
+
+    return 0;
+}
